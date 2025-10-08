edd1a1f10d28e323ac3fed3823a6d10852b3dc74
Muhammad Rehan Khan <extteomkh@fdm.dk>
2025-10-08 08:51:01 +0500

    initial

--- tests/example.spec.js
+++ tests/example.spec.js
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('searches for vehicle', async ({ page }) => {
   await page.goto('https://version2-develop.fdm.dk/vi-tilbyder/forsikring/bilreparation');
   await page.getByRole('button', { name: 'Accepter alle' }).click();
   await page.getByRole('textbox', { name: 'Indtast din nummerplade' }).click();
@@ -11,6 +11,22 @@ test('test', async ({ page }) => {
   await page.getByRole('textbox', { name: 'Hvor mange km har bilen kørt' }).click();
   await page.getByRole('textbox', { name: 'Hvor mange km har bilen kørt' }).fill('5000');
   await page.getByRole('button', { name: 'Næste' }).click();
+  await page.getByRole('button', { name: 'close-popover' }).click();
+  await page.getByRole('button', { name: 'Næste' }).click();
+  await page.goto('https://version2-develop.fdm.dk/bestil/medlemskab/');
+});
+
+test('searches for vehicle second fail', async ({ page }) => {
+  await page.goto('https://version2-develop.fdm.dk/vi-tilbyder/forsikring/bilreparation');
+  await page.getByRole('button', { name: 'Accepter alle' }).click();
+  await page.getByRole('textbox', { name: 'Indtast din nummerplade' }).click();
+  await page.getByRole('textbox', { name: 'Indtast din nummerplade' }).fill('DY69454');
+  await page.getByRole('button', { name: 'Søgee' }).click();
+  await page.getByRole('button', { name: 'close-popover' }).click();
+  await page.locator('label').filter({ hasText: 'Jeg bekræfter på tro & love:' }).click();
+  await page.getByRole('textbox', { name: 'Hvor mange km har bilen kørt' }).click();
+  await page.getByRole('textbox', { name: 'Hvor mange km har bilen kørt' }).fill('5000');
+  await page.getByRole('button', { name: 'Næsteee' }).click();
   await page.getByRole('button', { name: 'close-popover' }).click();
   await page.getByRole('button', { name: 'Næste' }).click();
   await page.goto('https://version2-develop.fdm.dk/bestil/medlemskab/');
